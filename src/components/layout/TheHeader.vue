04a14f1e34ace014b8bde27bb1d3f6858d870af2
Dilbek Alimurodov <emotifs@mail.ru>
2022-07-30 09:47:16 +0500

    changed header

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <header>
     <nav>
-      <h1><router-link to="/">Find a Coach</router-link></h1>
+      <h1><router-link to="/">Find a Coach App</router-link></h1>
       <ul>
         <li><router-link to="/coaches">All Coaches</router-link></li>
         <li><router-link to="/requests">Requests</router-link></li>
